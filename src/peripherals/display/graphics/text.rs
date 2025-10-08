ec86ee49ba0a56b7e3a2c49ef5ff22a8d8e87b25
Fernando Perez <fernando.perez.lara@proton.me>
2025-10-08 09:45:57 +0200

    chore(text): load new font

--- src/peripherals/display/graphics/text.rs
+++ src/peripherals/display/graphics/text.rs
@@ -1,14 +1,11 @@
 use alloc::string::String;
 
-pub use embedded_graphics::text::Alignment;
-use embedded_graphics::{
-    mono_font::{MonoTextStyle, ascii::FONT_10X20},
-    prelude::{Drawable as EgDrawable, Point},
-    text::{Baseline, LineHeight, Text as EgText, TextStyleBuilder},
-};
 use log::debug;
 
 use super::{Color, Display, Graphic};
+use crate::include_generated;
+
+const FONT_BITMAP: &[u8] = include_generated!(bytes: "fonts/primary.bin");
 
 pub struct Text {
     pub content: String,
@@ -23,18 +20,5 @@ impl Graphic for Text {
             "Drawing text '{}' at ({}, {}) with color {:?}",
             self.content, self.x, self.y, self.color
         );
-
-        let position = Point::new(self.x as i32, self.y as i32);
-        let color = self.color.into();
-        let character_style = MonoTextStyle::new(&FONT_10X20, color);
-        let text_style = TextStyleBuilder::new()
-            .alignment(Alignment::Center)
-            .baseline(Baseline::Middle)
-            .line_height(LineHeight::Percent(150))
-            .build();
-
-        let text = EgText::with_text_style(&self.content, position, character_style, text_style);
-
-        let _ = text.draw(display);
     }
 }
